2876958a64c01efdc0926a07699ec35508eb5d9f
Loic CHAPRENET <lchapren@e3r11p6.42.fr>
2020-04-02 17:21:59 +0200

    ft_strcmp finnaly correctly working

--- main.c
+++ main.c
@@ -22,8 +22,8 @@ void	test_ft_strcmp()
 	const char *s1;
 	const char *s2;
 
-	s1 = "abc";
-	s2 = "";
+	s1 = "ab";
+	s2 = "abc";
 	printf("s1 : [%s] \t s2 : [%s]\n", s1, s2);
 	printf("<string.h> strcmp function: [%d]\n", strcmp(s1, s2));
 	printf("'libasm.h' ft_strcmp function: [%d]\n", ft_strcmp(s1, s2));
--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -3,35 +3,22 @@
 global _ft_strcmp
 
 _ft_strcmp:
+	mov al, [rdi]
+	sub al, [rsi]		; diff stored in 1 byte reg
+
 	cmp [rdi], byte 0	; if the first string is done
 	je ft_strcmp_stop
 	cmp [rsi], byte 0	; if the second string is done
 	je ft_strcmp_stop
 	
-	cmp rdi, rsi		; compare string 1 byte to string 2 byte
+	cmp al, 0			; if there is a diff
 	jne ft_strcmp_stop
 	
 	inc rdi				; next rdi byte
 	inc rsi				; next rsi byte
-	jmp _ft_strcmp		; call next iteration on new byte
+	jmp _ft_strcmp		; call next iteration with new bytes
 
 
 ft_strcmp_stop:
-	mov rax, [rdi]		; storing first string
-	mov rbx, [rsi]		; storing second string
-	sub rax, rbx		; get difference
-
-	cmp rax, 0			; if diff < 0
-	jl ft_strcmp_less
-	cmp rax, 0			; if diff > 0
-	jg ft_strcmp_more
-	
-	ret					; rax == 0
-
-ft_strcmp_less:
-	mov rax, -1	; put -1 in return register
-	ret
-
-ft_strcmp_more:
-	mov rax, 1	; put 1 in return register
+	movsx rax, al		; putting back the diff in rax
 	ret
